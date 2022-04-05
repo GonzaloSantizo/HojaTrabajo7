9552cf5c565db541a425784acfcc20bb23673600
GonzaloSantizo <84192252+GonzaloSantizo@users.noreply.github.com>
2022-04-05 16:22:42 -0600

    Updated Main classes
    Added default words to main dictionaries

--- Main.java
+++ Main.java
@@ -1,17 +1,40 @@
 import java.util.Comparator;
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+
 
 public class Main{
 
     public static void main (String[] args) {
-        System.out.println("Create a tree");
-        BST<String, String> English = new BST<String, String>(new
-       AscendingStringComparator());
-       
-       English.add("Test1", "Prueba 1");
-       English.add("Test2", "Prueba 2");
-       English.add("Test4", "Prueba 4");
-       English.add("Test3", "Prueba 3");
+    System.out.println("Create a tree");
+    BST<String, String> English = new BST<String, String>(new AscendingStringComparator()); // create english BST
+    BST<String, String> French = new BST<String, String>(new AscendingStringComparator()); // create french BST
 
+    
+    // add english known words;
+    English.add("house", "Casa");
+    English.add("dog", "perro");
+    English.add("woman", "mujer");
+    English.add("homework", "tarea");
+    English.add("town", "pueblo");
+    English.add("yes", "si");
+    
+
+
+    // Add French known Words;
+
+    French.add("house", "loger");
+    French.add("dog", "chien");
+    French.add("woman", "fremme");
+    French.add("homework", "devoirs");
+    French.add("town", "ville");
+    French.add("yes", "oui");
+
+    /*
        System.out.println("In-order traverse the tree");
        English.traverse();
 
@@ -23,8 +46,50 @@ public class Main{
        
        English.remove("Test3");
        System.out.println("In-order traverse the tree after removing 'i'");
-       English.traverse();
+       English.traverse(); 
+       
+    */
+
+        Scanner scan = new Scanner(System.in);
+        // Crear perro y persona
+        int opcion = 0;
+        
+
+        while (opcion != 5) {
+
+            System.out.println("\n\nWelcome to the translator what are you triying to do?");
+            System.out.println("1. Translate a sentence");
+            System.out.println("2. Edit the value in a word");
+            System.out.println("3. Delete word from translator");
+            System.out.println("4. Add a word");
+            System.out.println("5. Show content on both dictionaries");
+            System.out.println("6. Salir\n\n");
+
+            opcion = scan.nextInt();
+
+            if (opcion == 1) {
+                // Translate a sentence
+  
+
+            } else if (opcion == 2) {
+                // Edit the value in a word
+            } else if (opcion == 3) {
+                // Delete word from translator
+            } else if (opcion == 4) {
+                // Add a word
+            } else if (opcion == 5) {
+                // Show content on both dictionaries
+
+                System.out.println("English dictionary:");
+                English.traverse();
+
+                System.out.println("\n\nFrench dictionary:");
+                French.traverse();
 
 
+            }else if (opcion == 6) {
+                // Exit
+            }
+        }
       }
 }
